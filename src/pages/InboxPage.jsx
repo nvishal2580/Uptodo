74c67bb980ab2e63a8782519c1baccef405c9070
vishal nagar <56758869+nvishal2580@users.noreply.github.com>
2022-03-26 16:16:55 +0530

    fix: bugs

--- src/pages/InboxPage.jsx
+++ src/pages/InboxPage.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import { doc, onSnapshot } from 'firebase/firestore';
+import React,{useState,useEffect} from 'react'
+import { auth, db } from '../services/firebase/firebase';
 
 function InboxPage() {
+
+ const [tasks,setTasks] = useState([]);
+
+  useEffect(()=>{
+    const unsub = onSnapshot(doc(db,'users',auth.currentUser.uid),(snapshot)=> {
+      const data = snapshot.data();
+      if(data === undefined) return ;
+       
+    })
+  })
+
   return (
     <div>InboxPage</div>
   )
