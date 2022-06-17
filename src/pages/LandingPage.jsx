df7de1a88a7c3f8b9d7dca2da0b8391fc2928fee
vishal nagar <56758869+nvishal2580@users.noreply.github.com>
2022-06-17 19:17:00 +0530

    add: firebase hosted

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import TeamImage from "../assets/logos/Team.jpg";
+import { Link } from "react-router-dom";
 
 function LandingPage() {
-    return <div className='text-center'>
-        <h1 className='text-3xl my-12'>welcome to Uptodo App</h1>
+  return (
+    <div className="w-screen h-screen overflow-hidden bg-[#FEFEFE]">
+      {/* <h1 className='text-3xl my-12'>welcome to Uptodo App</h1>
         <Link to='/login'>
             <button className='bg-indigo-500 m-5 px-4 py-2 text-white rounded font-medium uppercase shadow hover:bg-indigo-400'>
                 Login
@@ -13,8 +15,36 @@ function LandingPage() {
             <button className='bg-gray-500 m-5 px-4 py-2 text-white rounded font-medium uppercase shadow hover:bg-gray-400'>
                 Register
             </button>
+        </Link> */}
+      <div className="flex">
+        <div className="w-6/12 pl-32 pt-4">
+          <span className="font-extrabold underline decoration-[#211D40] font-mono text-6xl text-[#E62552]">
+            Uptodo
+          </span>
+          <div className="mt-20   text-7xl font-bold ">
+            
+            <span className="underline decoration-sky-500">A Collaborative </span>{" "}
+            <span className="underline decoration-yellow-500">Task</span>{" "}
+            <span className="underline decoration-red-500"> Mangaement</span> 
+            
+            <div className="underline decoration-[#3F964F]">Application</div>
+          </div>
+          <p className="mt-8 font-light text-lg">
+            Alternative way of organising and planning your work to make sure it
+            will be done.
+          </p>
+          <Link to='/login'>
+            <button className='bg-[#343CB2] mt-8 px-4 py-2 text-white rounded-full font-semibold hover:underline hover:decoration-red-500 shadow-lg'>
+                Gets Started
+            </button>
         </Link>
-    </div>;
+        </div>
+        <div className="w-6/12">
+          <img src={TeamImage} alt="team" className="bg-cover" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default LandingPage;
